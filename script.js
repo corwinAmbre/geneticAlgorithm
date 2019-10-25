607834fcfe850dc7030814b10367772461ab6bed
corwinAmbre <sebastien.domergue@gmail.com>
2019-10-25 17:55:54 +0200

    Adding first epoch to genetic flow, score sort and parallelize work. Breed method to be done

--- script.js
+++ script.js
@@ -10,11 +10,26 @@ function getRandomInt(max) {
 }
 
 class Genetic {
-	constructor(popSize, epochs) {
+	constructor(popSize, epochs, breedOptions) {
 		this.popSize = popSize;
 		this.epochs = epochs;
 		this.population = new Array();
 		this.trained = 0;
+		this.trainOnGoing = 0;
+		this.epoch = 0;
+		this.breedOptions = Object.assign({
+			keepTops: 1,
+			breedRate: 0.4
+		}, breedOptions);
+	}
+	
+	async run() {
+		this.createInitialPop();
+		await this.train();
+		for(var i = 1; i < this.epochs; i = i + 1) {
+			this.breed();
+			await this.train();
+		}
 	}
 	
 	createInitialPop() {
@@ -23,19 +38,28 @@ class Genetic {
 		}
 	}
 	
-	train() {
+	async train() {
+		this.epoch += 1;
 		this.trained = 0;
-		var $this = this;
-		var i = 1;
-		this.population.forEach(function(cnn) {
+		this.trainOnGoing = 0;
+		this.updateDisplay();
+		var promises = new Array();
+		for(var i = 0; i < this.popSize; i = i + 1) {
+			var cnn = this.population[i];
 			if(cnn.score == null) {
-				console.log("Training individual " + i++);
-				$this.trainIndividual(cnn);
+				console.log("Training individual " + (i+1));
+				promises.push(this.trainIndividual(cnn));
 			} else {
-				console.log("Skipping individual " + i++);
-				$this.trained += 1;
+				console.log("Skipping individual " + (i+1));
+				this.trained += 1;
+				this.updateDisplay();
 			}
+		}
+		await Promise.all(promises);
+		this.population.sort(function(a, b) {
+			return a.score - b.score;
 		});
+		return true;
 	}
 	
 	createIndividual() {
@@ -54,15 +78,33 @@ class Genetic {
 		return cnn;
 	}
 	
+	breed() {
+	}
+	
 	async trainIndividual(cnn) {
+		this.trainOnGoing += 1;
+		this.updateDisplay();
 		const data = new MnistData();
 		await data.load();
 		const model = cnn.getModel();
 		await cnn.train(model, data);
 		await cnn.showAccuracy(model, data);
 		this.trained += 1;
-		console.log("Number of trained: " + this.trained);
+		this.trainOnGoing -= 1;
+		this.updateDisplay();
 		console.log("Training complete: " + cnn.score);
+	}
+	
+	updateDisplay() {
+		$("#genetic-epoch").text(this.epoch);
+		var notStarted = this.popSize - this.trainOnGoing - this.trained;
+		$("#genetic-to-train").text(notStarted == 0 ? '' : notStarted);
+		$("#genetic-train-on-going").text(this.trainOnGoing == 0 ? '' : this.trainOnGoing);
+		$("#genetic-trained").text(this.trained == 0 ? '' : this.trained);
+		$("#genetic-to-train").css("witdh", notStarted * 400 / this.popSize + "px");
+		$("#genetic-train-on-going").css("width", this.trainOnGoing * 400 / this.popSize + "px");
+		$("#genetic-trained").css("width", this.trained * 400 / this.popSize + "px");
+		
 	}
 }
 
@@ -86,14 +128,12 @@ async function runRaw() {
 //document.addEventListener('DOMContentLoaded', runRaw);
 
 async function run() {
-	var genetic = new Genetic($("#setup-population-size").val(), $("#setup-max-round").val());
-	console.log("Create initial population");
-	genetic.createInitialPop();
-	console.log("Population created - " + genetic.population.length + " individuals");
-	genetic.train();
-	
+	var genetic = new Genetic($("#setup-population-size").val(), $("#setup-max-round").val(), {});
+	genetic.run();	
 }
 
 $("#launchBtn").on("click", function() {
+	$("#main-run").css("display", "flex");
+	$("#main-idle").css("display", "none");
 	run();
 });
